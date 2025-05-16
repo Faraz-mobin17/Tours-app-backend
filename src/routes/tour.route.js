836b2a9883ad79c7588f4bde3634f0f5ad5d6fef
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-05-16 14:18:29 +0530

    Refactor tour routes and update sorting log message format

--- src/routes/tour.route.js
+++ src/routes/tour.route.js
@@ -2,17 +2,21 @@ import Router from "express";
 import {
   createTour,
   deleteTour,
-  getParticularUser,
   updateTour,
   getAllTours,
+  getTourStats,
+  getParticularTour,
+  getMonthlyPlan,
 } from "../controllers/tour.controller.js";
 import { aliasTopTour } from "../middlewares/tour.middleware.js";
 const router = Router();
 router.route("/top-5-tours").get(aliasTopTour, getAllTours);
+router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 router.route("/").get(getAllTours).post(createTour);
 router
   .route("/:id")
-  .get(getParticularUser)
+  .get(getParticularTour)
   .patch(updateTour)
   .delete(deleteTour);
 
--- src/utils/ApiFeatures.js
+++ src/utils/ApiFeatures.js
@@ -19,7 +19,7 @@ class ApiFeatures {
   sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(",").join(" ");
-      console.log(sortBy);
+      console.log("SortBy: " + sortBy);
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort("-createdAt");
