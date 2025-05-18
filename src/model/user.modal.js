c833cd1684077f15c3a4bf2bbfb8701df1e485ef
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-05-19 00:22:59 +0530

    Add user model with schema validation and password confirmation

---
+++ src/model/user.modal.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
+    select: false, // Do not return password by default
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Password confirmation is required"],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords do not match",
+    },
+  },
+  photo: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
