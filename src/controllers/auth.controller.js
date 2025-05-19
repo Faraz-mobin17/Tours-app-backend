86172ce5997aa15f8a3507997bd8bc3425403de2
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-05-19 07:56:38 +0530

    🌚 Signup and login method completed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,13 +2,17 @@ import { User } from "../model/user.modal";
 import { asyncHandler } from "../middleware/asyncHandler";
 import { ApiError } from "../utils/apiError";
 import { ApiResponse } from "../utils/ApiResponse";
+import jwt from "jsonwebtoken";
+
 const signup = asyncHandler(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
   // check if user already exists
   const userExists = await User.findOne({ email });
+
   if (userExists) {
-    return next(new ApiError("User already exists", 400));
+    return next(new ApiError(400, "User already exists"));
   }
+
   // create user
   const user = await User.create({
     name,
@@ -16,8 +20,49 @@ const signup = asyncHandler(async (req, res, next) => {
     password,
     passwordConfirm,
   });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   // send response
-  res.status(201).json(new ApiResponse(201, user, "User created successfully"));
+  return res.status(201).json({
+    status: "success",
+    token,
+    message: "User created successfully",
+    data: {
+      user,
+    },
+  });
 });
 
-export { signup };
+const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if email and password are provided
+  if (!email || !password) {
+    return next(new ApiError(400, "Please provide email and password"));
+  }
+
+  // check if user exists
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new ApiError(401, "Incorrect email or password"));
+  }
+
+  // generate token
+  const token = undefined;
+
+  // send response
+  return res.status(200).json({
+    status: "success",
+    token,
+    message: "User logged in successfully",
+    data: {
+      user,
+    },
+  });
+});
+
+export { signup, login };
