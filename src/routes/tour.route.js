0e7db6bc46ef60fa9aaf29a0c99fb8bfbdec38ef
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-05-21 05:44:53 +0530

    Add authentication protection to getAllTours route

--- src/routes/tour.route.js
+++ src/routes/tour.route.js
@@ -9,11 +9,14 @@ import {
   getMonthlyPlan,
   aliasTopTour,
 } from "../controllers/tour.controller.js";
+
+import { protect } from "../controllers/auth.controller.js";
+
 const router = Router();
 router.route("/top-5-tours").get(aliasTopTour, getAllTours);
 router.route("/tour-stats").get(getTourStats);
 router.route("/monthly-plan/:year").get(getMonthlyPlan);
-router.route("/").get(getAllTours).post(createTour);
+router.route("/").get(protect, getAllTours).post(createTour);
 router
   .route("/:id")
   .get(getParticularTour)
