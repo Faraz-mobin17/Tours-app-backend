b3ad0947f74cd3c6fb0d9314882969d9edc7eee9
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-06-01 00:08:25 +0530

    🌚 updated roles

--- src/model/user.modal.js
+++ src/model/user.modal.js
@@ -35,7 +35,7 @@ const userSchema = new Schema({
   },
   role: {
     type: String,
-    enum: ["user", "admin"],
+    enum: ["user", "admin", "guide", "lead-guide"],
     default: "user",
   },
   createdAt: {
