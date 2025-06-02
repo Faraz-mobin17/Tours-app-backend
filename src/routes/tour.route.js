7995cb7e5e8525de1e8074c22e0b7952d9e7648a
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-06-02 22:45:11 +0530

    🌚 minor changes

--- src/routes/tour.route.js
+++ src/routes/tour.route.js
@@ -12,15 +12,17 @@ import {
 
 import { protect, restrict } from "../controllers/auth.controller.js";
 
-const router = Router();
-router.route("/top-5-tours").get(aliasTopTour, getAllTours);
-router.route("/tour-stats").get(getTourStats);
-router.route("/monthly-plan/:year").get(getMonthlyPlan);
-router.route("/").get(protect, getAllTours).post(createTour);
-router
-  .route("/:id")
-  .get(getParticularTour)
-  .patch(updateTour)
-  .delete(protect, restrict("admin", "lead-guide"), deleteTour);
+export default function tourRouter() {
+  const router = Router();
+  router.route("/top-5-tours").get(aliasTopTour, getAllTours);
+  router.route("/tour-stats").get(getTourStats);
+  router.route("/monthly-plan/:year").get(getMonthlyPlan);
+  router.route("/").get(protect, getAllTours).post(createTour);
+  router
+    .route("/:id")
+    .get(getParticularTour)
+    .patch(updateTour)
+    .delete(protect, restrict("admin", "lead-guide"), deleteTour);
 
-export default router;
+  return router;
+}
