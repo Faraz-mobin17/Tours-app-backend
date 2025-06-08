ec0295111dbf2c989384a51a495a6169d203a8b5
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-06-08 20:40:20 +0530

    ✅ send email func completed

---
+++ src/utils/email.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async (options) => {
+  // 1) create a transporter
+  // Looking to send emails in production? Check out our Email API/SMTP product!
+  var transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  // 2) Define the email options
+
+  const mailOptions = {
+    from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_FROM}>`, // sender address
+    to: options.email, // list of receivers
+    subject: options.subject, // Subject line
+    text: options.message, // plain text body
+    html: options.html, // html body
+  };
+
+  // 3) actuall send the email
+  const response = await transport.sendMail(mailOptions);
+  // 4) log the response
+  if (response.accepted.length > 0) {
+    console.log("Email sent successfully:", response);
+  } else {
+    console.error("Failed to send email:", response);
+  }
+};
+
+export { sendEmail as default };
