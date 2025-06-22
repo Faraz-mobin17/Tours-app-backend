c637b7a90ca3469d81424f542742e8931b1c9f8d
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-06-22 22:06:43 +0530

    ✅minor imports fixes

--- src/routes/tour.route.js
+++ src/routes/tour.route.js
@@ -7,14 +7,14 @@ import {
   getTourStats,
   getParticularTour,
   getMonthlyPlan,
-  aliasTopTour,
+  aliasTopTours,
 } from "../controllers/tour.controller.js";
 
 import { protect, restrict } from "../controllers/auth.controller.js";
 
 export default function tourRouter() {
   const router = Router();
-  router.route("/top-5-tours").get(aliasTopTour, getAllTours);
+  router.route("/top-5-tours").get(aliasTopTours, getAllTours);
   router.route("/tour-stats").get(getTourStats);
   router.route("/monthly-plan/:year").get(getMonthlyPlan);
   router.route("/").get(protect, getAllTours).post(createTour);
