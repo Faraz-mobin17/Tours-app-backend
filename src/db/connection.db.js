1b21a689410c65b8d9b374dca60424918215b174
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-07-14 01:16:22 +0530

    🌚 added env imoprts

--- src/db/connection.db.js
+++ src/db/connection.db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import envVars from "../../config/serverConfig.js";
+import { env } from "../../config/serverConfig.js";
 import { DB_NAME } from "../utils/constants.js";
 
 const connectDB = async (
-  connectionString = `${envVars.MONGODB_URI}/${DB_NAME}` ||
+  connectionString = `${env.MONGODB_URI}/${DB_NAME}` ||
     `mongodb://127.0.0.1:27017/${DB_NAME}`
 ) => {
   try {
