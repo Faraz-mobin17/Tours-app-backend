608b3437d8bfd23049ecb2836fc7c540128c9482
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-07-14 01:15:04 +0530

    🌚 added single exports file

---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+// In your export file
+export { authController } from "./auth.controller.js";
+export { reviewController } from "./review.controller.js";
+export { tourController } from "./tour.controller.js";
+export { userController } from "./user.controller.js";
---
+++ src/routes/index.js
@@ -0,0 +1,3 @@
+export { tourRouter } from "./tour.route.js";
+export { userRouter } from "./user.route.js";
+export { reviewRouter } from "./review.route.js";
