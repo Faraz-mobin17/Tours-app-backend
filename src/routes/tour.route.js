700674b2ebaa906d4b51bb52ef966520bb98b6f2
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-07-14 01:14:21 +0530

    🌚 refactored tour route

--- src/routes/tour.route.js
+++ src/routes/tour.route.js
@@ -1,28 +1,36 @@
 import Router from "express";
-import {
-  createTour,
-  deleteTour,
-  updateTour,
-  getAllTours,
-  getTourStats,
-  getParticularTour,
-  getMonthlyPlan,
-  aliasTopTours,
-} from "../controllers/tour.controller.js";
 
-import { protect, restrict } from "../controllers/auth.controller.js";
+import { authController, tourController } from "../controllers/index.js";
 
-export default function tourRouter() {
-  const router = Router();
-  router.route("/top-5-tours").get(aliasTopTours, getAllTours);
-  router.route("/tour-stats").get(getTourStats);
-  router.route("/monthly-plan/:year").get(getMonthlyPlan);
-  router.route("/").get(protect, getAllTours).post(createTour);
-  router
-    .route("/:id")
-    .get(getParticularTour)
-    .patch(updateTour)
-    .delete(protect, restrict("admin", "lead-guide"), deleteTour);
+import { reviewRouter } from "./review.route.js";
 
-  return router;
-}
+console.log("inside tour router");
+
+const tourRouter = Router();
+
+tourRouter.use("/:tourId/reviews", reviewRouter);
+
+tourRouter
+  .route("/top-5-tours")
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+tourRouter.route("/tour-stats").get(tourController.getTourStats);
+
+tourRouter.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
+
+tourRouter
+  .route("/")
+  .get(authController.protect, tourController.getAllTours)
+  .post(tourController.createTour);
+
+tourRouter
+  .route("/:id")
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(
+    // authController.protect,
+    authController.restrict("admin", "lead-guide"),
+    tourController.deleteTour
+  );
+
+export { tourRouter };
