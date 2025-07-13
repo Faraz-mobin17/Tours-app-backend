5b290ef5111e1c455108ae8f9805bf7383db5987
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-07-14 01:11:09 +0530

    🌚 wrote better code

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,21 +1,26 @@
-import fs from "fs";
 import dotenv from "dotenv";
-// import * as url from "url";
-// const __filename = url.fileURLToPath(import.meta.url);
-// const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-// console.log(__dirname);
-// console.log(__filename);
-import { dotenvPath } from "../path.js";
-if (fs.existsSync(dotenvPath)) {
-  dotenv.config(dotenvPath);
-} else {
-  console.error(".env file not found");
-  process.exit();
+
+export function loadEnv() {
+  const result = dotenv.config();
+
+  if (result.error) {
+    throw new Error("failed to load dotenv");
+  }
+  console.log("dot env loaded successfully");
 }
+loadEnv();
 
-const envVars = {
-  PORT: process.env.PORT || 5000,
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017",
+export const env = {
+  PORT: process.env.PORT || 3000,
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/",
+  NODE_ENV: process.env.NODE_ENV || "developement",
+  JWT_SECRET: process.env.JWT_SECRET || "secret",
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "90d",
+  JWT_COOKIE_EXPIRES_IN: process.env.JWT_COOKIE_EXPIRES_IN || "90",
+  EMAIL_USERNAME: process.env.EMAIL_USERNAME,
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_PORT: process.env.EMAIL_PORT || 25,
+  EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME || "Faraz",
+  EMAIL_FROM: process.env.EMAIL_FROM,
 };
-
-export default envVars;
