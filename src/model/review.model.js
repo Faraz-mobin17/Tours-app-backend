225076d71abc39301ecdd354a8a9989c89d6e0ca
Faraz-mobin17 <faraz.mobin17@gmail.com>
2025-07-13 13:13:30 +0530

    🌚 added find middleware to populate user and tour

--- src/model/review.model.js
+++ src/model/review.model.js
@@ -1,27 +1,44 @@
 import mongoose, { Schema } from "mongoose";
-const reviewSchema = new Schema({
-  review: {
-    type: String,
+const reviewSchema = new Schema(
+  {
+    review: {
+      type: String,
+    },
+    ratings: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Types.ObjectId,
+      ref: "Tour",
+      required: [true, "Tour should exists for reviewsSchema"],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "User must exists for review Schema"],
+    },
   },
-  ratings: {
-    type: Number,
-    min: 1,
-    max: 5,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  tour: {
-    type: mongoose.Types.ObjectId,
-    ref: "Tour",
-    required: [true, "Tour should exists for reviewsSchema"],
-  },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: [true, "User must exists for review Schema"],
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "tour",
+    select: "name",
+  }).populate({
+    path: "user",
+    select: "name photo",
+  });
+  next();
 });
 
 export const reviewModel = mongoose.model("Review", reviewSchema);
